dc137701594448d09befe07aae0217fb1f2af65e
tyange <usun16@gmail.com>
2023-05-18 21:03:43 +0900

    black magic for using programmatic navigation

--- pages/clients/[id]/[clientsprojectid].js
+++ pages/clients/[id]/[clientsprojectid].js
@@ -1,8 +1,6 @@
 import { useRouter } from "next/router";
 
 function SelectedClientProjectPage() {
-  const router = useRouter();
-
   return (
     <div>
       <h1>The Project Page for a Specific Project for a Selected Client</h1>
--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,18 @@
+import { useRouter } from "next/router";
+
 function ClientProjectsPage() {
+  const router = useRouter();
+
+  function loadProjectHandler() {
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "tyange1", clientprojectid: "projecta" },
+    });
+  }
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
