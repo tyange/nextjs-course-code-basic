f0ce105b6afd1bcfb270d39493acae7dc079ba3e
tyange <usun16@gmail.com>
2023-05-18 20:55:07 +0900

    black magic for using Link

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,28 @@
+import Link from "next/link";
+
 function ClientsPage() {
+  const clients = [
+    { id: "ty1", name: "tyange1" },
+    { id: "ty2", name: "tyange2" },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link
+              href={{
+                pathname: "/clients/[id]",
+                query: { id: client.id },
+              }}
+            >
+              {client.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
